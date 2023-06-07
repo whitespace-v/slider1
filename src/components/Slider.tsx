2260e26da065df41b44c6f20e9209478905053e9
whitespace-v <grs2143@mail.ru>
2023-06-07 15:53:17 +0300

    dd

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -58,86 +58,90 @@ const Slider = () => {
                 <motion.div
                     ref={containerRef}
                     viewport={{once: true}}
-                    initial={{x: -100, opacity: 0}}
-                    whileInView={{x: 0, opacity: 1}}
+                    initial={{ opacity: 0}}
+                    whileInView={{ opacity: 1}}
                     transition={{duration: 1}}
                 >
-                    <Swiper
-                        onRealIndexChange={(element) => setCurrent(element.activeIndex)}
-                        slidesPerView={3}
-                        spaceBetween={120}
-                        centeredSlides={true}
-                        onSwiper={(swiper) => setSwiperInstance(swiper)}
-                        modules={[Navigation]}
-                        //width of the container in css must be adapted to project's settings!
-                        // then breakpoints will be flexible
-                        breakpoints={{
-                            300: {slidesPerView: 1},
-                            650:  {slidesPerView: 2},
-                            1000: {slidesPerView: 3},
-                            1300: {slidesPerView: 4}
-                        }}
-                    >
-                        {carData.map((i,idx) =>
-                            <SwiperSlide className={classes['Slider__item']}
-                            >
-                                <Transition in={idx===current} timeout={200}>
-                                    { contentState =>
-                                        <div className={classes['Slider__item-content'] + ' ' + classes[contentState]}>
-                                            <Transition in={idx===current} timeout={200} mountOnEnter unmountOnExit>
-                                                { state =>
-                                                    <div className={classes['Slider__item-content-left'] + ' ' + classes[state]}
-                                                         style={{backgroundImage: `url(${letter})`}}
-                                                    >
-                                                        <div className={classes['Slider__item-content-left-title']}>
-                                                            <div className={classes['Slider__item-content-left-title-text']}>
-                                                                <b>{i.name}</b>
-                                                            </div>
-                                                            <div className={classes['Slider__item-content-left-title-price']}>
-                                                                от <b>{i.price}</b>
-                                                            </div>
-                                                        </div>
-                                                        <div className={classes['Slider__item-content-left-button'] + ' ' + classes[state]}>
-                                                            Перейти в каталог <BsArrowRight/>
-                                                        </div>
-                                                        {width < 600 &&
-                                                            <div className={classes['Slider__item-content-left-button-more']}>
-                                                                Еще +
-                                                            </div>
-                                                        }
-                                                    </div>
-                                                }
-                                            </Transition>
-                                            <Transition in={idx===current} timeout={500}>
-                                                { state =>
-                                                    <div className={classes['Slider__item-content-car'] + ' ' + classes[state]}
-                                                         style={{backgroundImage: `url(${i.image})`}}
-                                                    />
-                                                }
-                                            </Transition>
-                                        </div>
-                                    }
-                                </Transition>
-                            </SwiperSlide>
-                        )}
-                    </Swiper>
+                    <div className={classes['Slider__plate']}>
+                        <div className={classes['Slider__plate-left']}
+                             style={{backgroundImage: `url(${letter})`}}
+                        >
+                            <div className={classes['Slider__plate-left-title']}>
+                                <div className={classes['Slider__plate-left-title-name']}>
+                                    <b>{carData[current].name}</b>
+                                </div>
+                                <div className={classes['Slider__plate-left-title-price']}>
+                                    от <b>{carData[current].price}</b>
+                                </div>
+                            </div>
+                            <div className={classes['Slider__plate-left-button']}>
+                                Перейти в каталог <BsArrowRight/>
+                            </div>
+                        </div>
+                        <div className={classes['Slider__plate-right']}>
+                            {width < 600 &&
+                                <div className={classes['Slider__plate-more']}>
+                                    Еще +
+                                </div>
+                            }
+                            {width > 600 &&
+                                <motion.div className={classes['Slider__plate-control']}
+                                            initial={'hidden'} whileInView={'visible'} custom={1} variants={itemAnimation}
+                                >
+                                    <div className={classes['Slider__plate-control-prev']}
+                                         onClick={() => swiperInstance.slidePrev()}
+                                    >
+                                        <SlArrowLeft/>
+                                    </div>
+                                    <div className={classes['Slider__plate-control-next']}
+                                         onClick={() => swiperInstance.slideNext()}
+                                    >
+                                        <SlArrowRight/>
+                                    </div>
+                                </motion.div>
+                            }
+                        </div>
+
+                    </div>
+                    <div className={classes['Slider-container']}>
+                        <Swiper
+                            onRealIndexChange={(element) => setCurrent(element.activeIndex)}
+                            centeredSlides={true}
+                            initialSlide={1}
+                            onSwiper={(swiper) => setSwiperInstance(swiper)}
+                            modules={[Navigation]}
+                            //width of the container in css must be adapted to project's settings!
+                            // then breakpoints will be flexible
+                            breakpoints={{
+                                300: {
+                                    slidesPerView: 1
+                                },
+                                600:  {
+                                    slidesPerView: 3,
+                                    spaceBetween: 100
+                                },
+                                800: {
+                                    slidesPerView: 3,
+                                    spaceBetween: 80
+                                }
+                            }}
+                        >
+                            {carData.map((i,idx) =>
+                                <SwiperSlide className={classes['Slider__item']}>
+                                    <div className={classes['Slider__item-content']}>
+                                        <Transition in={idx===current} timeout={500}>
+                                            { state =>
+                                                <div className={classes['Slider__item-content-car'] + ' ' + classes[state]}
+                                                     style={{backgroundImage: `url(${i.image})`}}
+                                                />
+                                            }
+                                        </Transition>
+                                    </div>
+                                </SwiperSlide>
+                            )}
+                        </Swiper>
+                    </div>
                 </motion.div>
-                {width > 600 &&
-                    <motion.div className={classes['Slider__control']}
-                                initial={'hidden'} whileInView={'visible'} custom={1} variants={itemAnimation}
-                    >
-                        <div className={classes['Slider__control-prev']}
-                             onClick={() => swiperInstance.slidePrev()}
-                        >
-                            <SlArrowLeft/>
-                        </div>
-                        <div className={classes['Slider__control-next']}
-                             onClick={() => swiperInstance.slideNext()}
-                        >
-                            <SlArrowRight/>
-                        </div>
-                    </motion.div>
-                }
             </AnimatePresence>
 
 
--- src/data.ts
+++ src/data.ts
@@ -9,7 +9,7 @@ export interface ICarData{
 }
 export const carData = [
     {name: 'Hyundai GENESIS X', price: '2 000 000', image: image1, filter: '/hyundai_gs_x'},
-    {name: 'Toyota Land Cruiser 200', price: '5 600 000', image: image2, filter: '/toyota_lc_200'},
+    {name: 'Toyota LC 200', price: '5 600 000', image: image2, filter: '/toyota_lc_200'},
     {name: 'Lexus GS460', price: '2 400 000', image: image3, filter: '/lexus_gs_460'},
     {name: 'Lexus IS350H', price: '4 700 000', image: image4, filter: '/lexus_is_350h'},
     {name: 'Lexus LX600', price: '18 200 000', image: image5, filter: '/lexus_lx_600'},
